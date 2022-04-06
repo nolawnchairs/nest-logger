77b2aff67430360ffb49e3aceb7992da42b4a433
nolawnchairs <nolawnchairs@gmail.com>
2022-04-06 17:21:05 +0700

    allow function (class) to be passed as value

--- src/logger/logger.decorator.ts
+++ src/logger/logger.decorator.ts
@@ -3,7 +3,8 @@ import { Inject } from '@nestjs/common'
 import { addLoggingContext } from './logger.provider'
 import { NEST_LOGGER_PREFIX } from './logger.config'
 
-export function Logger(context: string) {
+export function Logger(contextOrClass: string | Function) {
+  const context = typeof contextOrClass == 'string' ? contextOrClass : contextOrClass.name
   addLoggingContext(context)
   return Inject(`${NEST_LOGGER_PREFIX}.${context}`)
 }
