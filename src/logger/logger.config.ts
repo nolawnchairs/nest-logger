4fabf1b25af6b112d177f3228ddaf20a56ede839
nolawnchairs <nolawnchairs@gmail.com>
2022-08-16 02:20:59 +0700

    add docs to config

--- src/logger/logger.config.ts
+++ src/logger/logger.config.ts
@@ -8,22 +8,86 @@ export const NEST_LOGGER_CONFIG = 'NEST_LOGGER_CONFIG'
 export const DEFAULT_CONTEXT = 'Main'
 
 export interface ILoggerConfig {
+  /**
+   * The default context for the logger, if none is provided
+   * via injection. If omitted, no context will be printed.
+   *
+   * @type {string}
+   * @memberof ILoggerConfig
+   */
   defaultContext?: string
   profiles: {
+    /**
+     * The configuration for the file logger
+     *
+     * @type {(ILoggingProfileOptions & IFileLoggingOptions)}
+     */
     file: ILoggingProfileOptions & IFileLoggingOptions
+    /**
+     * The configuration for the console logger
+     *
+     * @type {ILoggingProfileOptions}
+     */
     stdout: ILoggingProfileOptions
   }
 }
 
 export interface ILoggingProfileOptions {
+  /**
+   * The log levels to apply to the logger.
+   *
+   * Can be an array of NestJS log levels, as is normally
+   * provided to the native NestJS logger:
+   *
+   * ```ts
+   * ['verbose', 'debug', 'log', 'warn', 'error']
+   * ```
+   *
+   * Or a combined string shorthand:
+   *
+   * ```
+   * VDIWE (verbose, debug, info, warn, error)
+   * ```
+   * Note that the level for `log` is abbreviated as `I` and not `L`
+   *
+   * @type {(LogLevel[] | string)}
+   * @memberof ILoggingProfileOptions
+   */
   level: LogLevel[] | string
+  /**
+   * Whether or not to enable this logging profile. Default `true`.
+   *
+   * @type {boolean}
+   * @memberof ILoggingProfileOptions
+   */
   enabled?: boolean
+  /**
+   * The end-of-line character to use when writing logs. Defaluts to `\n`.
+   *
+   * @type {string}
+   * @memberof ILoggingProfileOptions
+   */
   eol?: string
 }
 
 export type IFileLoggingOptions = {
+  /**
+   * The full path to the log file.
+   *
+   * @type {string}
+   */
   filename: string
+  /**
+   * The file mode for the log file. Defaults to `644`.
+   *
+   * @type {number}
+   */
   mode?: number
+  /**
+   * The character encoding for the log file. Defaults to `utf8`.
+   *
+   * @type {BufferEncoding}
+   */
   encoding?: BufferEncoding
 }
 
