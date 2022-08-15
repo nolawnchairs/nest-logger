f44ce79aac190126f3a777828a30f3bd2f9413b7
nolawnchairs <nolawnchairs@gmail.com>
2022-08-16 02:25:03 +0700

    add doc to decorator

--- src/logger/logger.decorator.ts
+++ src/logger/logger.decorator.ts
@@ -3,6 +3,12 @@ import { Inject } from '@nestjs/common'
 import { addLoggingContext } from './logger.provider'
 import { NEST_LOGGER_PREFIX } from './logger.config'
 
+/**
+ * Decorator to inject a new instance of the LoggerService
+ *
+ * @export
+ * @param {(string | Function)} contextOrClass - The context to use for the logger as a string or a class constructor, from which the name will be derived.
+ */
 export function Logger(contextOrClass: string | Function) {
   const context = typeof contextOrClass == 'string' ? contextOrClass : contextOrClass.name
   addLoggingContext(context)
