7758df366d1356dd653b233a3ee907f9720895cf
nolawnchairs <nolawnchairs@gmail.com>
2023-03-09 12:39:26 +0700

    moved decorators to own dir

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import { ILoggerConfig, LoggerConfig, NEST_LOGGER } from './logger/logger.config'
-import { Logger } from './logger/logger.decorator'
+import { Logger } from './logger/decorators/logger.decorator'
 import { LoggerModule } from './logger/logger.module'
 import { LoggerService } from './logger/logger.service'
 
