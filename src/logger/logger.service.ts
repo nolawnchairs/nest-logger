9072d3a709a8c8c8fac43f1e494f28bf6f6fac3f
nolawnchairs <nolawnchairs@gmail.com>
2023-07-18 17:35:15 +0700

    fix logging issues proper positions on error calls fix deferred eval

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -13,7 +13,6 @@ export class LoggerService extends ConsoleLogger {
    * Logger service. Logs LOGGING_LEVEL to stdout, but only logs error and warn
    * log events to LOG_FILE
    *
-   * @param {string} [context]
    * @memberof LoggerService
    */
   constructor(
@@ -26,92 +25,107 @@ export class LoggerService extends ConsoleLogger {
    * Log a VERBOSE level message with deferred evaluation
    *
    * @param {MessageSupplier} messageSupplier the message supplier function to be evaluated if VERBOSE level is enabled
-   * @param {string} [context] optional context
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  verbose(messageSupplier: MessageSupplier, context?: string): void
+  verbose(messageSupplier: MessageSupplier, ...optionalParams: any[]): void
   /**
    * Log a VERBOSE level message
    *
-   * @param {*} message the message to be logged
-   * @param {string} [context] optional context
+   * @param {string} message the message to be logged
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  verbose(message: string, context?: string): void
-  verbose(message: any, context?: string): void {
-    if (this.isFileLoggingEnabled('verbose'))
-      this.dump(message, 'VERB', context)
-    if (this.config.stdout.enabled)
-      super.verbose.call(this, (typeof message == 'function' ? message() : message), context)
+  verbose(message: string, ...optionalParams: any[]): void
+  verbose(message: any, ...optionalParams: any[]): void {
+    // Although the underlying console-logger checks the log level, we must
+    // check it here as well to prevent unnecessary evaluation of the message
+    // supplier function
+    if (this.isConsoleLoggingEnabled('verbose')) {
+      // Although context is intrinsic to the instance, arguments passed to all
+      // logging methods can override the logging context with the last
+      // argument, so we must pass it explicitly as the last argument to the
+      // super call. This affects all logging methods below.
+      super.verbose(typeof message == 'function' ? message() : message, ...optionalParams, this.context)
+    }
+    if (this.isFileLoggingEnabled('verbose')) {
+      this.dump(message, 'VERB', undefined, ...optionalParams)
+    }
   }
 
   /**
    * Log a DEBUG level message with deferred evaluation
    *
    * @param {MessageSupplier} messageSupplier the message supplier function to be evaluated if DEBUG level is enabled
-   * @param {string} [context]
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  debug(messageSupplier: MessageSupplier, context?: string): void
+  debug(messageSupplier: MessageSupplier, ...optionalParams: any[]): void
   /**
    * Log a DEBUG level message
    *
    * @param {*} message the message to be logged
-   * @param {string} [context] optional context
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  debug(message: string, context?: string): void
-  debug(message: any, context?: string): void {
-    if (this.isFileLoggingEnabled('debug'))
-      this.dump(message, 'DEBUG', context)
-    if (this.config.stdout.enabled)
-      super.debug.call(this, (typeof message == 'function' ? message() : message), context)
+  debug(message: string, ...optionalParams: any[]): void
+  debug(message: any, ...optionalParams: any[]): void {
+    if (this.isConsoleLoggingEnabled('debug')) {
+      super.debug(typeof message == 'function' ? message() : message, ...optionalParams, this.context)
+    }
+    if (this.isFileLoggingEnabled('debug')) {
+      this.dump(message, 'DEBUG', undefined, ...optionalParams)
+    }
   }
 
   /**
-   * Log a INFO level message with deferred evaluation
+   * Log a INFO/LOG level message with deferred evaluation
    *
    * @param {MessageSupplier} messageSupplier the message supplier function to be evaluated if INFO level is enabled
-   * @param {string} [context] optional context
+   * @param {...any[]} optionalParams optional parameters to include
    * @memberof LoggerService
    */
-  log(messageSupplier: MessageSupplier, context?: string): void
+  log(messageSupplier: MessageSupplier, ...optionalParams: any[]): void
   /**
    * Log a INFO level message
   *
-  * @param {*} message
-  * @param {string} [context]
+  * @param {*} message the message to be logged
+  * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
   * @memberof LoggerService
   */
-  log(message: string, context?: string): void
-  log(message: any, context?: string): void {
-    if (this.isFileLoggingEnabled('log'))
-      this.dump(message, 'INFO', context)
-    if (this.config.stdout.enabled)
-      super.log.call(this, (typeof message == 'function' ? message() : message), context)
+  log(message: string, ...optionalParams: any[]): void
+  log(message: any, ...optionalParams: any[]): void {
+    if (this.isConsoleLoggingEnabled('log')) {
+      super.log(typeof message == 'function' ? message() : message, ...optionalParams, this.context)
+    }
+    if (this.isFileLoggingEnabled('log')) {
+      this.dump(message, 'INFO', undefined, ...optionalParams)
+    }
   }
 
   /**
    * Log a WARN level message with deferred evaluation
    *
    * @param {MessageSupplier} messageSupplier the message supplier function to be evaluated if WARN level is enabled
-   * @param {string} [context]
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  warn(messageSupplier: MessageSupplier, context?: string): void
+  warn(messageSupplier: MessageSupplier, ...optionalParams: any[]): void
   /**
    * Log a WARN level message
    *
-   * @param {*} message
-   * @param {string} [context]
+   * @param {string} message the message to be logged
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  warn(message: string, context?: string): void
-  warn(message: any, context?: string): void {
-    if (this.isFileLoggingEnabled('warn'))
-      this.dump(message, 'WARN', context)
-    if (this.config.stdout.enabled)
-      super.warn.call(this, (typeof message == 'function' ? message() : message), context)
+  warn(message: string, ...optionalParams: any[]): void
+  warn(message: any, ...optionalParams: any[]): void {
+    if (this.isConsoleLoggingEnabled('warn')) {
+      super.warn(typeof message == 'function' ? message() : message, ...optionalParams, this.context)
+    }
+    if (this.isFileLoggingEnabled('warn')) {
+      this.dump(message, 'WARN', undefined, ...optionalParams)
+    }
   }
 
   /**
@@ -119,24 +133,51 @@ export class LoggerService extends ConsoleLogger {
    *
    * @param {MessageSupplier} messageSupplier the message supplier function to be evaluated if ERROR level is enabled
    * @param {string} [stack] optional stack trace
-   * @param {string} [context] optional context
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  error(messageSupplier: MessageSupplier, stack?: string, context?: string): void
+  error(messageSupplier: MessageSupplier, stack?: string, ...optionalParams: any[]): void
   /**
    * Log an ERROR level message
    *
    * @param {*} message the message to be logged
    * @param {string} [stack] optional stack trace
-   * @param {string} [context] optional context
+   * @param {...any[]} optionalParams optional parameters to include (each param will print to its own line)
    * @memberof LoggerService
    */
-  error(message: string, stack?: string, context?: string): void
-  error(message: any, stack?: string, context?: string): void {
-    if (this.isFileLoggingEnabled('error'))
-      this.dump(message, 'ERROR', stack)
-    if (this.config.stdout.enabled) {
-      super.error.call(this, (typeof message == 'function' ? message() : message), stack, context)
+  error(message: string, stack?: string, ...optionalParams: any[]): void
+  error(message: any, stack?: string, ...optionalParams: any[]): void {
+    if (this.isConsoleLoggingEnabled('error')) {
+      const printMessage = typeof message == 'function' ? message() : message
+      if (this.isStackLike(stack)) {
+        // NestJS's positional arguments are a bit weird, so we have to do some
+        // extra work to ensure the stack trace is printed as such... If stack
+        // and optional params are passed in the wrong order, we will get odd
+        // printing behavior (like the stack trace being printed as a
+        // stringified object, or a non-stack item being printed naked to the
+        // console as would be expected with a stack trace)
+        //
+        // Explicitly pass context as with the other methods, but ensure
+        // argument[1] is treated as the stack trace if it is stack-like, and
+        // explicitly set as the next-to-last param as expected by the super
+        // call. Optional params in arguments[2...] are then passed as expected.
+        super.error(printMessage, ...optionalParams, stack, this.context)
+      } else {
+        // If the 1st argument is not stack-like, we can treat it like any other
+        // param and pass it before the next-to-last param explicitly as
+        // undefined so the super call sees it as such and will not print it.
+        // Optional params are then passed as expected, as is explicit context.
+        super.error(printMessage, stack, ...optionalParams, undefined, this.context)
+      }
+    }
+
+    // The file logging API is a bit more sensical
+    if (this.isFileLoggingEnabled('error')) {
+      if (this.isStackLike(stack)) {
+        this.dump(message, 'ERROR', stack, ...optionalParams)
+      } else {
+        this.dump(message, 'ERROR', undefined, stack, ...optionalParams)
+      }
     }
   }
 
@@ -147,16 +188,31 @@ export class LoggerService extends ConsoleLogger {
    * @param {string} [stack]
    * @memberof LoggerService
    */
-  private dump(message: any, level: string, stack?: string) {
+  private dump(message: any, level: string, stack?: string, ...optionalParams: any[]) {
     this.writer.write(
       JSON.stringify({
         date: new Date().toISOString(),
         context: this.context,
         level,
         message: typeof message == 'function' ? message() : message,
-        stack,
+        // Ensure stack, if null, is undefined so it is not serialized
+        stack: stack == 'string' ? stack.split('\n').map(s => s.trim()) : undefined,
+        params: optionalParams?.length ? optionalParams : undefined,
       }) + this.config.file.eol,
     )
+  }
+
+  /**
+   * Determine if console logging is enabled for a given level
+   *
+   * @private
+   * @param {LogLevel} level
+   * @return {*}  {boolean}
+   * @memberof LoggerService
+   */
+  private isConsoleLoggingEnabled(level: LogLevel): boolean {
+    return this.config.stdout.enabled
+      && this.config.stdout.level.includes(level)
   }
 
   /**
@@ -164,10 +220,23 @@ export class LoggerService extends ConsoleLogger {
    *
    * @private
    * @param {LogLevel} level
+   * @return {*}  {boolean}
    * @memberof LoggerService
    */
-  private isFileLoggingEnabled(level: LogLevel) {
+  private isFileLoggingEnabled(level: LogLevel): boolean {
     return this.config.file.enabled
       && this.config.file.level.includes(level)
+  }
+
+  /**
+   * Detect if a string is probably a stack trace
+   *
+   * @private
+   * @param {string} value the string to test
+   * @return {*}  {boolean}
+   * @memberof LoggerService
+   */
+  private isStackLike(value: string): boolean {
+    return typeof value == 'string' && /(.)+\n\s+at .+:\d+:\d+/.test(value)
   }
 }
